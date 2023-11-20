b25d6be9c979b76eb5bd9276518a20c69370933b
sohailnajar147 <sohailnajar147@gmail.com>
2023-11-20 14:02:33 +0100

    bidha

---
+++ app/page.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="space-y-10 text-[#565869] h-screen flex flex-col justify-center items-center  ">
+      <h1 className="font-bold text-3xl">ChatGPT</h1>
+
+      <div className="grid grid-cols-2 grid-rows-2 gap-3">
+        <div className="infoText">
+          <p className="text-white">Explain nostalgia</p>
+          <p>for what to do with kids art</p>
+        </div>
+        <div className="infoText">
+          <p className="text-white">Explain nostalgia</p>
+          <p>for what to do with kids art</p>
+        </div>
+        <div className="col-start-2 row-start-2 infoText">
+          <p className="text-white">Explain nostalgia</p>
+          <p>for what to do with kids art</p>
+        </div>
+        <div className="col-start-1 row-start-2 infoText">
+          <p className="text-white">Explain nostalgia</p>
+          <p>for what to do with kids art</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default page;
---
+++ components/SideBar.tsx
@@ -0,0 +1,15 @@
+import NewChat from "./NewChat";
+
+function SideBar() {
+  return (
+    <div className="p-2 flex flex-col h-screen ">
+      <div className="flex-1">
+        <div>
+          <NewChat />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SideBar;
