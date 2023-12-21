e02e88c3c58b6cdf339722466ebb26079c23c22e
sohailnajar147 <sohailnajar147@gmail.com>
2023-12-21 17:18:55 +0100

    final

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -2,17 +2,21 @@
 import Chat from "@/components/Chat";
 import ChatInput from "@/components/ChatInput";
 import Loading from "@/components/Loading";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
+
 type Props = {
   params: {
     id: string;
   };
 };
+
 function ChatPage({ params: { id } }: Props) {
+  const pathname = usePathname();
   const [hidden, setHidden] = useState<boolean>(true);
   return (
     <div className="flex flex-col  h-screen  ">
-      <Chat chatId={id} hidden={hidden} />
+      {<Chat chatId={id} hidden={hidden} />}
       <ChatInput chatId={id} setHidden={setHidden} />
     </div>
   );
